2e2661e34c0f85b9bb710de5bf0a3d894a970571
Teddy <megareda1998@gmail.com>
2020-03-12 11:40:08 +0100

    Ajout route de test

--- api/src/server.ts
+++ api/src/server.ts
@@ -4,8 +4,31 @@ import * as Router from 'koa-router';
 const app = new Koa();
 const router = new Router();
 
-router.get('/*', async (ctx) => {
-    ctx.body = 'Salut tout le monde';
+router.get('/api', async ctx => {
+    ctx.body = 'Hello, World!';
+});
+
+router.get('/bonjour', async context => {
+    const obj: any = [
+        {
+            'userId': 34,
+            'identity': {
+                'name': 'Teddy',
+                'surname': 'Sommavilla',
+                'age': 21,
+                'localisation': 'Grenoble'
+            },
+            'stuff': [
+                'yes',
+                'no',
+                'maybe',
+                'I dont know',
+                'can you repeat the question?'
+            ]
+        },
+    ];
+
+    context.body = obj;
 });
 
 app.use(router.routes());
