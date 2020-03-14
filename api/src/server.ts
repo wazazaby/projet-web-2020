379f17d7e1a5f2bf9d3983f1db4edfbf7d0bc652
Teddy <megareda1998@gmail.com>
2020-03-15 00:56:32 +0100

    Ajout du bodyparser pour Koa + test routes

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,9 +1,9 @@
 import * as Koa from 'koa';
 import * as cors from 'koa-cors';
 import * as logger from 'koa-logger';
+import * as koaBody from 'koa-body';
 
 import rootRouter from './routes/test.routes';
-import kevinRouter from './routes/kevin.routes';
 
 const app = new Koa();
 const PORT = 3000;
@@ -12,12 +12,12 @@ const PORT = 3000;
 app.use(cors());
 app.use(logger());
 
+// Permet de parse les données envoyées en POST
+app.use(koaBody());
+
 // ---------- ROUTES ----------
 app.use(rootRouter.routes());
 app.use(rootRouter.allowedMethods());
-
-app.use(kevinRouter.routes());
-app.use(kevinRouter.allowedMethods());
 // ---------- ROUTES ----------
 
 // Lancement du serveur sur le port 3000 et log l'adresse de l'API pour que ce soit moins chiant
