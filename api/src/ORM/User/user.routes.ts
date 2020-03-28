641e3b8fc0aee5920bec0449ef4ec6bb14f0220c
Teddy <megareda1998@gmail.com>
2020-03-28 16:16:51 +0100

    Création de la route d'insert user + ajout des templates des futurs routes

--- api/src/ORM/User/user.routes.ts
+++ api/src/ORM/User/user.routes.ts
@@ -5,10 +5,24 @@ import { User } from './Entity/User';
 
 const router = new Router();
 
-router.get('/api/inscription', async (ctx: Context) => {
-    const acc: CreateAccount = new CreateAccount();
-    const test: User = await acc.getResult();
-	ctx.body = test;
+const API_USER = '/api/user';
+
+// router.get('/api/test/:token', async (ctx: Context) => {
+//     const acc: CreateAccount = new CreateAccount();
+//     const test: User = await acc.getResult();
+//     ctx.body = test;
+//     ctx.body = ctx.params;
+// });
+
+router.post(`${API_USER}/inscription`, async (ctx: Context) => {
+    const acc: CreateAccount = new CreateAccount(ctx);
+    await acc.getResult();
 });
+
+router.get(`${API_USER}/activation/:token`, async (ctx: Context) => {});
+
+router.post(`${API_USER}/authentification`, async (ctx: Context) => {});
+
+router.get(`${API_USER}/deconnexion`, async (ctx: Context) => {});
 
 export default router;
