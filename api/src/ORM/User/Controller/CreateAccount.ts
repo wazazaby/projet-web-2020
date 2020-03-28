c7c91f7c68c87615eb35c4014484c8b6a14f09e4
Teddy <megareda1998@gmail.com>
2020-03-28 16:17:53 +0100

    Insertion d'user depuis les données reçus en POST

--- api/src/ORM/User/Controller/CreateAccount.ts
+++ api/src/ORM/User/Controller/CreateAccount.ts
@@ -1,11 +1,44 @@
 import { ManagerUser } from "../Manager/ManagerUser";
 import { Context } from 'koa'; 
 import { User } from "../Entity/User";
+import { UserInterface } from "../../../interfaces";
 
 export class CreateAccount {
-    public async getResult (): Promise<User> {
-        const managerUser: ManagerUser = new ManagerUser();
-        const user: User = await managerUser.getUserByMail('test@mail.com');
-        return user;
+    private _ctx: Context;
+
+    constructor (ctx: Context) {
+        this._ctx = ctx;
+    }
+
+    public async getResult (): Promise<void> {
+        const dataUser: UserInterface = {
+            id_user: null,
+            name_user: this._ctx.request.body.name,
+            email_user: this._ctx.request.body.email,
+            pass_user: this._ctx.request.body.pass,
+            actif_user: 0,
+            rgpd_user: this._ctx.request.body.rgpd,
+            token_user: '1q2w3e4r5t6y7u8i9o0p',
+            create_date_user: Math.floor(Date.now() / 1000),
+            modification_date_user: null
+        };
+
+        const newUser: User = new User(dataUser);
+
+        const manager: ManagerUser = new ManagerUser();
+        const check: User | null = await manager.getUserByMail(newUser.email);
+
+        if (check == null) {
+            const result: any = await manager.insertUser(newUser);
+            
+            if (result.affectedRows == 1 && result.insertId > 0) {
+                // User inséré, envoie mail d'activation
+                this._ctx.body = result;
+            } else {
+                // Echec insert
+            }
+        } else {
+            // Utilisateur éxiste déjà
+        }
     }
 }
