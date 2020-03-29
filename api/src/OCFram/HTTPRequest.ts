3837d8a88f0485bdb81a60e76e6a9450b768a624
Teddy <megareda1998@gmail.com>
2020-03-29 01:04:35 +0100

    Modification des routes + suppression fichier

--- api/src/OCFram/HTTPRequest.ts
+++
@@ -1,9 +0,0 @@
-import { Context } from 'koa';
-
-class HTTPRequest {
-    public static async getDataGet (name: string, ctx: Context): Promise<any> {
-        // if (ctx.request.body.hasOwnProperty(name)) {
-
-        // }
-    }
-}
--- api/src/ORM/User/user.routes.ts
+++ api/src/ORM/User/user.routes.ts
@@ -1,28 +1,20 @@
 import * as Router from 'koa-router';
 import { Context } from 'koa';
 import { CreateAccount } from './Controller/CreateAccount';
-import { User } from './Entity/User';
 
 const router = new Router();
 
 const API_USER = '/api/user';
 
-// router.get('/api/test/:token', async (ctx: Context) => {
-//     const acc: CreateAccount = new CreateAccount();
-//     const test: User = await acc.getResult();
-//     ctx.body = test;
-//     ctx.body = ctx.params;
-// });
-
-router.post(`${API_USER}/inscription`, async (ctx: Context) => {
+router.post(`${API_USER}/inscription`, async (ctx: Context): Promise<void> => {
     const acc: CreateAccount = new CreateAccount(ctx);
     await acc.getResult();
 });
 
-router.get(`${API_USER}/activation/:token`, async (ctx: Context) => {});
+router.get(`${API_USER}/activation/:token`, async (ctx: Context): Promise<void> => {});
 
-router.post(`${API_USER}/authentification`, async (ctx: Context) => {});
+router.post(`${API_USER}/authentification`, async (ctx: Context): Promise<void> => {});
 
-router.get(`${API_USER}/deconnexion`, async (ctx: Context) => {});
+router.get(`${API_USER}/deconnexion`, async (ctx: Context): Promise<void> => {});
 
 export default router;
