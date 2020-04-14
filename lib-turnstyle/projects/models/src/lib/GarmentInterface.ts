626e033c6c9f4498b30a8851f0942c6da4e11060
Teddy <megareda1998@gmail.com>
2020-04-14 17:04:19 +0200

    Modification interface de retour pour garment

--- lib-turnstyle/projects/models/src/lib/GarmentInterface.ts
+++ lib-turnstyle/projects/models/src/lib/GarmentInterface.ts
@@ -2,14 +2,15 @@ import { ColorInterface } from './ColorInterface';
 import { StyleInterface } from './StyleInterface';
 
 export interface GarmentInterface {
-  id_garment: number;
-  label_garment: string;
-  url_img_garment: string;
-  creation_date_garment: number;
-  modification_date_garment: number;
-  user_id_user: number;
-  brand_id_brand: number;
-  season_id_season: number;
-  style?: StyleInterface[];
-  color?: ColorInterface[];
+    id_garment?: number;
+    label_garment: string;
+    url_img_garment: string;
+    creation_date_garment: number;
+    modification_date_garment?: number;
+    user_id_user: number;
+    brand_id_brand: number;
+    season_id_season: number;
+    type_id_type: number;
+    garment_has_style?: StyleInterface[];
+    garment_has_color?: ColorInterface[];
 }
