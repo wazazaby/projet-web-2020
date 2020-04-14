31c554c36c25ba5c1ed6ea32754478b92d4eebfc
Teddy <megareda1998@gmail.com>
2020-04-14 14:17:45 +0200

    Fix interface user

--- lib-turnstyle/projects/models/src/public-api.ts
+++ lib-turnstyle/projects/models/src/public-api.ts
@@ -2,3 +2,4 @@ export * from './lib/UserInterface';
 export * from './lib/RouteInterface';
 export * from './lib/ColorInterface';
 export * from './lib/InsertReturnInterface';
+export * from './lib/GlobalReturnInterface';
