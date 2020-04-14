c03aaa87d2e8adc5f5f1c55361254c9128215212
klandry <kevinlandry38@gmail.com>
2020-04-14 10:05:08 +0200

    Maj lib: interface
    Ajoute les interfaces: -Garment, -Style

--- lib-turnstyle/projects/models/src/public-api.ts
+++ lib-turnstyle/projects/models/src/public-api.ts
@@ -2,3 +2,5 @@ export * from './lib/UserInterface';
 export * from './lib/RouteInterface';
 export * from './lib/ColorInterface';
 export * from './lib/InsertReturnInterface';
+export * from './lib/GarmentInterface';
+export * from './lib/StyleInterface';
