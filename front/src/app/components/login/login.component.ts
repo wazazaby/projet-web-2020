0331cff01e72bdc851451c36917687c0b962116d
klandry <kevinlandry38@gmail.com>
2020-04-15 09:23:20 +0200

    Ajoute message d'erreur

--- front/src/app/components/login/login.component.ts
+++ front/src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,8 +10,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit { // contient les var du component
 
   formSubmit: FormGroup = this.formBuild.group({
-    email: ['testTonMail@turnstyle.com', Validators.required],
-    password: ['123456', [Validators.required, Validators.minLength(8)]]
+    email: ['testTonMail@turnstyle.com', Validators.compose([
+      Validators.required,
+      Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+    ])],
+    password: ['12345678', [Validators.required, Validators.minLength(8)]]
   });
 
   submitted = false;
@@ -21,7 +24,7 @@ export class LoginComponent implements OnInit { // contient les var du component
 }
 
   ngOnInit() {
-
+    // console.log(this.formSubmit);
   }
 
   onLogin() {
@@ -29,7 +32,6 @@ export class LoginComponent implements OnInit { // contient les var du component
 
     // Data des inputs
     // this.formSubmit.value.email or this.formSubnmit.value.password
-
     console.log(this.formSubmit);
   }
 
