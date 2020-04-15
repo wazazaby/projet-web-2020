dbadff0298d21037822710015c6fb84b41e785bf
Teddy <megareda1998@gmail.com>
2020-04-15 13:43:29 +0200

    Fix GarmentInterface

--- lib-turnstyle/projects/models/src/lib/GarmentInterface.ts
+++ lib-turnstyle/projects/models/src/lib/GarmentInterface.ts
@@ -1,16 +1,14 @@
-import { ColorInterface } from './ColorInterface';
-import { StyleInterface } from './StyleInterface';
-
-export interface GarmentInterface {
-    id_garment?: number;
-    label_garment: string;
-    url_img_garment: string;
-    creation_date_garment: number;
-    modification_date_garment?: number;
-    user_id_user: number;
-    brand_id_brand: number;
-    season_id_season: number;
-    type_id_type: number;
-    garment_has_style?: StyleInterface[];
-    garment_has_color?: ColorInterface[];
+import { ColorInterface } from './ColorInterface'; 
+import { StyleInterface } from './StyleInterface';  
+export interface GarmentInterface {   
+    id_garment?: number;   
+    label_garment: string;   
+    url_img_garment: string;   
+    creation_date_garment: number;   
+    modification_date_garment: number;   
+    user_id_user: number;   
+    brand_id_brand: number;   
+    season_id_season: number;   
+    style?: StyleInterface[];  
+    color?: ColorInterface[]; 
 }
