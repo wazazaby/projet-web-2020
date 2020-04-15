a74e155d82932760024e63972a73eda3510bbf45
klandry <kevinlandry38@gmail.com>
2020-04-15 14:09:09 +0200

    Lib: Ajoute de nouvelles interface

--- lib-turnstyle/projects/models/src/lib/InsertReturnInterface.ts
+++
@@ -1,8 +0,0 @@
-export interface InsertReturnInterface {
-    fieldcout: number;
-    affectedRows: number;
-    insertId: number;
-    info: string;
-    serverStatus: number;
-    warningStatus: number;
-}
---
+++ lib-turnstyle/projects/models/src/lib/OutfitInterface.ts
@@ -0,0 +1,7 @@
+export interface OutfitInterface {
+  id_outfit?: number;
+  label_outfit: string;
+  creation_date_outfit: number;
+  modification_date_outfit: number;
+  user_id_user: number;
+}
