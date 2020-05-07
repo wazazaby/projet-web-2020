3e6d0611bae707693ce1e632599526877b4180fd
klandry <kevinlandry38@gmail.com>
2020-05-08 00:07:35 +0200

    Maj des commentaire et réorganisation du fichier bridge.service

--- front/src/app/services/bridge.service.ts
+++ front/src/app/services/bridge.service.ts
@@ -17,165 +17,197 @@ import { Observable } from 'rxjs';
 })
 export class BridgeService {
 
-  constructor(private http: HttpClient,
-              private stateService: StatesService) { }
+    constructor(private http: HttpClient,
+                private stateService: StatesService) { }
 
-  // url pour API node
-  public brandAll = 'brand/all';
-  public seasonAll = 'season/all';
-  public typeAll = 'type/all';
-  public colorAll = 'color/all';
-  public userGarment = '/garment/all';
+    // url pour API node
+    public brandAll = 'brand/all';
+    public seasonAll = 'season/all';
+    public typeAll = 'type/all';
+    public colorAll = 'color/all';
+    public userGarment = '/garment/all';
 
-  /**
-   * Exemple crud: actuelement Read
-   * Récupére les data de la route /users
-   * Le resultat de cette fonction est obligatoirement un 'UserModel'
-   */
-  // getUser() {
-  //   return this.http.get<UserInterface>(environment.apiUrl + 'users');
-  // }
-
-  /**
-   * Connexion
-   * Permet d'identifier l'utilisateur
-   * @return GlobalReturnInterface
-   */
-  login(email: string, pass: string) {
-    const body = {
-        email,
-        pass
-      };
-    return this.http.post<GlobalReturnInterface>(environment.apiUrl + 'user/login', body);
-  }
-
-  initData(b: boolean): boolean {
-    let isGood = false;
-    if (b) {
-      console.log('init les data de l\'application', b);
-      this.getBrand();
-      this.getSeason();
-      this.getType();
-      this.getColor();
-
-      if (this.stateService.brand && this.stateService.season && this.stateService.type && this.stateService.color) {
-        isGood = true;
-      }
+/*
+ ******************************* function d'auth *******************************
+ */
+    /**
+     * Connexion
+     * Permet d'identifier l'utilisateur
+     * @return GlobalReturnInterface
+     */
+    login(email: string, pass: string) {
+        const body = {
+            email,
+            pass
+        };
+        return this.http.post<GlobalReturnInterface>(environment.apiUrl + 'user/login', body);
     }
-    return isGood;
-  }
 
-  /**
-   * Attribue les marques de vêtement à la var global
-   */
-  getBrand() {
-    return this.getBrandReq().subscribe(res => {
-      if (this.stateService.checkStatus(res.status)) {
-        const data: BrandInterface[] = res.data;
-        this.stateService.brand = data;
-      } else {
-        const err: ErrorInterface = {code: res.status, message: res.message, route: environment.apiUrl + this.brandAll};
-        this.stateService.errors = err;
-      }
-    });
-  }
+    /**
+     * Inscription
+     */
+    register() {
+        console.log('register');
+    }
 
-  /**
-   * Récupére toute les marques de vêtement
-   */
-  getBrandReq() {
-    return this.http.get<GlobalReturnInterface>(environment.apiUrl + this.brandAll);
-  }
+// ****************************************************************************************
 
-  /**
-   * Attribue les saisons à la var global
-   */
-  getSeason() {
-    return this.getSeasonReq().subscribe(res => {
-      if (this.stateService.checkStatus(res.status)) {
-        const data: SeasonInterface[] = res.data;
-        this.stateService.season = data;
-      } else {
-        const err: ErrorInterface = {code: res.status, message: res.message, route: environment.apiUrl + this.seasonAll};
-        this.stateService.errors = err;
-      }
-    });
-  }
 
-  /**
-   * Récupére toute les saisons
-   */
-  getSeasonReq() {
-    return this.http.get<GlobalReturnInterface>(environment.apiUrl + this.seasonAll);
-  }
+    /**
+     * Permet de charger l'emsemble des data necessaire à l'APP
+     * @param b: boolean
+     */
+    initData(b: boolean): boolean {
+        let isGood = false;
+        if (b) {
+            console.log('init les data de l\'application', b);
+            this.getBrand();
+            this.getSeason();
+            this.getType();
+            this.getColor();
 
-  /**
-   * Attribue les types de vêtement à la var global
-   */
-  getType() {
-    return this.getTypeReq().subscribe(res => {
-      if (this.stateService.checkStatus(res.status)) {
-        const data: TypeInterface[] = res.data;
-        this.stateService.type = data;
-      } else {
-        const err: ErrorInterface = {code: res.status, message: res.message, route: environment.apiUrl + this.typeAll};
-        this.stateService.errors = err;
-      }
-    });
-  }
+            if (this.stateService.brand && this.stateService.season && this.stateService.type && this.stateService.color) {
+                isGood = true;
+            }
+        }
+        return isGood;
+    }
 
-  /**
-   * Récupére toute les types de vêtement
-   */
-  getTypeReq() {
-    return this.http.get<GlobalReturnInterface>(environment.apiUrl + this.typeAll);
-  }
 
-  getGarmentUser(userId: number) {
-    return this.getGarmentUserReq(userId).subscribe(res => {
-      if (this.stateService.checkStatus(res.status)) {
-        const data: GarmentColorStyleWrapperInterface[] = res.data;
-        this.stateService.garment = data;
-      } else {
-        const err: ErrorInterface = {
-          code: res.status,
-          message: res.message,
-          route: environment.apiUrl + 'user/' + userId + this.userGarment
-        };
-        this.stateService.errors = err;
-      }
-    });
-  }
+/*
+ *******************************alimente les variables glabal*******************************
+ */
 
-  getGarmentUserReq(userId: number) {
-    return this.http.get<GlobalReturnInterface>(environment.apiUrl + 'user/' + userId + this.userGarment);
-  }
+    /**
+     * Attribue les types de vêtement à la variable global {this.stateService.type}
+     */
+    getType() {
+        return this.getTypeReq().subscribe(res => {
+        if (this.stateService.checkStatus(res.status)) {
+            const data: TypeInterface[] = res.data;
+            this.stateService.type = data;
+        } else {
+            const err: ErrorInterface = {code: res.status, message: res.message, route: environment.apiUrl + this.typeAll};
+            this.stateService.errors = err;
+        }
+        });
+    }
 
-  getColor() {
-    return this.getColorReq().subscribe(res => {
-      if (this.stateService.checkStatus(res.status)) {
-        const data: ColorInterface[] = res.data;
-        this.stateService.color = data;
-      } else {
-        const err: ErrorInterface = {
-          code: res.status,
-          message: res.message,
-          route: environment.apiUrl + this.colorAll
-        };
-        this.stateService.errors = err;
-      }
-    });
-  }
+    /**
+     * Attribue les couleurs de vêtement à la variable global {this.stateService.color}
+     */
+    getColor() {
+        return this.getColorReq().subscribe(res => {
+            if (this.stateService.checkStatus(res.status)) {
+                const data: ColorInterface[] = res.data;
+                this.stateService.color = data;
+            } else {
+                const err: ErrorInterface = {
+                    code: res.status,
+                    message: res.message,
+                    route: environment.apiUrl + this.colorAll
+                };
+                this.stateService.errors = err;
+            }
+        });
+    }
 
-  getColorReq() {
-    return this.http.get<GlobalReturnInterface>(environment.apiUrl + this.colorAll);
-  }
+    /**
+     * Attribue les vétements à la variable global {this.stateService.garment}
+     * @param userId: number
+     */
+    getGarmentUser(userId: number) {
+        return this.getGarmentUserReq(userId).subscribe(res => {
+            if (this.stateService.checkStatus(res.status)) {
+                const data: GarmentColorStyleWrapperInterface[] = res.data;
+                this.stateService.garment = data;
+            } else {
+                const err: ErrorInterface = {
+                code: res.status,
+                message: res.message,
+                route: environment.apiUrl + 'user/' + userId + this.userGarment
+                };
+                this.stateService.errors = err;
+            }
+        });
+    }
 
-  /**
-   * Inscription
-   */
-  register() {
-    console.log('register');
-  }
+    /**
+     * Attribue les saisons à la variable global {this.stateService.season}
+     */
+    getSeason() {
+        return this.getSeasonReq().subscribe(res => {
+            if (this.stateService.checkStatus(res.status)) {
+                const data: SeasonInterface[] = res.data;
+                this.stateService.season = data;
+            } else {
+                const err: ErrorInterface = {code: res.status, message: res.message, route: environment.apiUrl + this.seasonAll};
+                this.stateService.errors = err;
+            }
+        });
+    }
+
+    /**
+     * Attribue les marques de vêtement à la variable global {this.stateService.brand}
+     */
+    getBrand() {
+        return this.getBrandReq().subscribe(res => {
+            if (this.stateService.checkStatus(res.status)) {
+                const data: BrandInterface[] = res.data;
+                this.stateService.brand = data;
+            } else {
+                const err: ErrorInterface = {code: res.status, message: res.message, route: environment.apiUrl + this.brandAll};
+                this.stateService.errors = err;
+            }
+        });
+    }
+
+
+// ****************************************************************************************
+
+
+/*
+ *******************************Requete simple*******************************
+ */
+
+    /**
+     * Créer un observable des couleurs de vêtement à la var global
+     */
+    getColorReq() {
+        return this.http.get<GlobalReturnInterface>(environment.apiUrl + this.colorAll);
+    }
+
+    /**
+     * Créer un observable des types de vêtement
+     */
+    getTypeReq() {
+        return this.http.get<GlobalReturnInterface>(environment.apiUrl + this.typeAll);
+    }
+
+    /**
+     * Créer un observable des vêtements d'un user
+     * @param userId: number
+     */
+    getGarmentUserReq(userId: number) {
+        return this.http.get<GlobalReturnInterface>(environment.apiUrl + 'user/' + userId + this.userGarment);
+    }
+
+    /**
+     * Créer un observable des saisons
+     */
+    getSeasonReq() {
+        return this.http.get<GlobalReturnInterface>(environment.apiUrl + this.seasonAll);
+    }
+
+    /**
+     * Créer un observable des marques de vêtement
+     */
+    getBrandReq() {
+        return this.http.get<GlobalReturnInterface>(environment.apiUrl + this.brandAll);
+    }
+
+
+
+// ****************************************************************************************
 
 }
