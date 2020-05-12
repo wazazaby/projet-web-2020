967b5480808f8f483a3d2a67dbe06222194dd8e3
klandry <kevinlandry38@gmail.com>
2020-05-12 20:33:06 +0200

    Api: ajoute mot de passe (ajouter DB_PASSWORD au .env)

--- api/src/libs/Db.ts
+++ api/src/libs/Db.ts
@@ -7,6 +7,7 @@ class Connection {
         const connection: Pool = await createPool({
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
             waitForConnections: true,
             connectionLimit: 20,
