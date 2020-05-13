5f19deca17c611a09941c0fc8bba0aebffb79799
klandry <kevinlandry38@gmail.com>
2020-05-13 18:34:39 +0200

    Front: Setup env prod

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+    // url de l'API node
+    apiUrl: 'http://turnstyle.klandry.fr:3000/api/',
+    // token d'identification pour l'API
+    tokenApi: 'token',
+    // url de base de l'application
+    baseUrl: 'http://turnstyle.klandry.fr',
 };
