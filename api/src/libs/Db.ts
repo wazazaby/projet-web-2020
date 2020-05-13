9d05838714fada04152d2275c14af126bd4ccbe8
Teddy <megareda1998@gmail.com>
2020-05-13 23:29:29 +0200

    Passage en synchrone

--- api/src/libs/Db.ts
+++ api/src/libs/Db.ts
@@ -3,19 +3,19 @@ import { Pool, createPool } from 'mysql2/promise';
 class Connection {
     public pool: Pool;
 
-    async init (message?: string): Promise<void> {
-        const connection: Pool = await createPool({
+    public init (message?: string): void {
+        const connection: Pool = createPool({
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
+            database: process.env.DB_NAME || '',
             waitForConnections: true,
             connectionLimit: 20,
             queueLimit: 0
         });
 
         this.pool = connection;
-        console.log(message ? message : 'http://localhost/phpmyadmin');
+        console.log(message);
     }
 }
 
