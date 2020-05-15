1616606a81a0225b62bc0bbf2ce4b2aa19535e2e
Teddy <megareda1998@gmail.com>
2020-05-16 01:03:18 +0200

    Passage du serving dans une libs

---
+++ api/src/libs/StaticServe.ts
@@ -0,0 +1,19 @@
+import * as Router from 'koa-router';
+import * as serve from 'koa-send';
+import { Context, DefaultState } from 'koa';
+
+const router: Router<DefaultState, Context> = new Router<DefaultState, Context>();
+router.get(
+    
+    // Match n'importe quelle url qui commence par /uploads/... 
+    '/uploads/(.*)', 
+    async (ctx: Context): Promise<string> => serve(
+        ctx, 
+        ctx.path,
+
+        // On stock les images dans les cache pendant 1 an
+        { maxAge: 31536000000 }
+    )
+);
+
+export default router;
