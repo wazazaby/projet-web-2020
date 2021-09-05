952080d238f2331e44d47b8f1dee910989bc4367
Teddy Sommavilla <36576716+Wazazaby@users.noreply.github.com>
2021-09-05 22:17:54 +0200

    Update BrandController.ts

--- api/src/main/brand/BrandController.ts
+++ api/src/main/brand/BrandController.ts
@@ -6,17 +6,14 @@ import { BrandInterface } from '@osmo6/models';
 export class BrandController {
     private _manager: BrandManager;
 
-    constructor () {
+    constructor() {
         this._manager = new BrandManager();
     }
 
-    async getAllBrand (ctx: Context): Promise<void> {
-        const result: BrandInterface[] = await this._manager.getAllBrand();
-        if (result) {
-            ctx.body = new Body(200, 'Ok', result);
-        } else {
-            ctx.body = new Body(403, "Aucune marque de vêtement n'a été trouver");
-        }
+    async getAllBrand(ctx: Context): Promise<void> {
+        const result: BrandInterface[] = await this._manager.getAllBrand();        
+        ctx.body = result.length > 0 
+            ? new Body(200, 'Ok', result) 
+            : new Body(403, 'Aucune marque n\'a été trouvée');
     }
-
 }
